b613c2bda9a6bc8c494e1966428c67294be7601f
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-23 13:49:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const dbUri = process.env.MONGO_URI;
+var user_dao = require('./models/user_dao');
 
 
 
