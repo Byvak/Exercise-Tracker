0b6f08a9a6877f3c27ce5b17ccb3b337ebc15a62
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-24 09:29:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,6 @@ app.post('/api/users/:_id/exercises', (req, res) => {
 
   user_dao.saveUserExercise(userId, exercises_entity, (result) => {
     if (result) {
-      res.json({
         username: result.username, _id: result._id
       });
     } else {
