7dd578ff3cdc721dafbe7246b8a610f91ab6fb00
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-25 16:16:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ app.post('/api/users', (req, res) => {
 
 app.post('/api/users/:_id/exercises', (req, res) => {
   var userId = req.params._id;
-  exercises_entity.date = req.body.date;
 
   //First we check that description, duration and date are not empty
 
