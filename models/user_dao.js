c964f0eba6b8337c3b13e66e5661ffe856191527
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-26 09:49:42 +0100

    Update user_dao.js

--- models/user_dao.js
+++ models/user_dao.js
@@ -7,6 +7,7 @@ module.exports.saveUser = function (username, callback) {
     User.create({ username: username }, function (err, doc) {
         if (err) {
             callback(false, "Oups an error " + err);
+        } else {
             callback(true, doc);
         }
     });
