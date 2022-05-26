cc64e3bd4454f757a22c54ceb2e15ab0111360db
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-26 09:06:19 +0100

    Update user_entity.js

--- models/entities/user_entity.js
+++ models/entities/user_entity.js
@@ -4,3 +4,4 @@ var Schema = mongoose.Schema;
 module.exports.UserEntity = function UserEntity() {
     return new Schema({
         username: String,
+        count: { type: Number, required: true, default: 0 },
