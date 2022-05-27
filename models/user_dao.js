dbdac8f7d96b27a9866f971fff12a95e9cf6b185
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-27 08:29:18 +0100

    Update user_dao.js

--- models/user_dao.js
+++ models/user_dao.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const UserEntity = require('./entities/user_entity').UserEntity();
 const User = mongoose.model('User', UserEntity);
 
-        if (err) {
             callback(false, "Oups an error " + err);
         } else {
             callback(true, doc);
