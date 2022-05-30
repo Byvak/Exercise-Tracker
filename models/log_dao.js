e364c057956e847781dfd7d2352ff1b99801d6eb
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-05-30 10:19:06 +0100

    Update log_dao.js

--- models/log_dao.js
+++ models/log_dao.js
@@ -2,3 +2,4 @@ const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 const LogEntity = require('./entities/log_entity').LogEntity();
 const Log = mongoose.model('Log', LogEntity);
+
