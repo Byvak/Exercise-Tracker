0e42edebf32efcd62de8d78443c9b33e1399977d
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-06-03 09:15:47 +0100

    Update exercise_dao.js

--- models/exercise_dao.js
+++ models/exercise_dao.js
@@ -2,3 +2,4 @@ const mongoose = require('mongoose');
 const ExerciseEntity = require('./entities/exercise_entity').ExerciseEntity();
 const Exercise = mongoose.model('Exercise', ExerciseEntity);
 
+module.exports.saveExercise = async function (exercise, callback) {
