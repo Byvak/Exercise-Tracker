b71da5304d058e4927238e857f8bb1fed3913c97
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-06-09 08:32:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -123,6 +123,7 @@ app.get('/api/users/:id/logs', (req, res) => {
             });
           } else {
             res.json({
+              Error: "Error :" + userExercisesFound
 app.post('/api/users', (req, res) => {
   var username = req.body.username;
   user_dao.saveUser(username, (etat, obj) => {
