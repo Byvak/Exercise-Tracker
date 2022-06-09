3d481fd09861296d73aee73bd612d2c8c3b25d13
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-06-09 08:32:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ app.get('/api/users/:id/logs', (req, res) => {
               log: logsArray
             }
             //We send back data after saving logs
+            log_dao.saveLog(logs, (etat, logsSaved) => {
 app.post('/api/users', (req, res) => {
   var username = req.body.username;
   user_dao.saveUser(username, (etat, obj) => {
